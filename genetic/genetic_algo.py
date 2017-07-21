ffac86d6adfb06e33d728c6206ecdf85ca017550
binking <jiangzhibin2014.xujie@gmail.com>
2017-07-21 21:41:48 +0800

    debug

--- genetic/genetic_algo.py
+++ genetic/genetic_algo.py
@@ -1,1 +1,10 @@
 # -*- coding: utf8-*-
+import sys
+
+
+class GeneticAlgo:
+    def __inti__(self, mutate_rate=0.2, cross_rate=0.4):
+        self.mutation_rate = mutate_rate
+        self.crossover_rate = cross_rate
+        self.current_fitness = sys.maxint
+        self.current_solution =
